533d51752a1dec1e4534e41387e72702701b51fe
Braxton Harley <braxton.2012.harley@gmail.com>
2020-12-30 13:57:51 -0600

    added styles.js file

--- src/components/Products/Product/Product.jsx
+++ src/components/Products/Product/Product.jsx
@@ -2,11 +2,33 @@ import React from 'react';
 import { Card, CardMedia, CardContent, CardActions, Typography, IconsButton } from '@material-ui/core';
 import { AddShoppingCart }  from '@material-ui/icons';
 
-const Product = () => {
+import useStyles from './styles';
+
+const Product = ( product ) => {
+    const classes = useStyles();
+    
     return (
-        <div>
-            
-        </div>
+       <Card className={classes.root}>
+           <CardMedia className={classes.media} image='' title={product.name} />
+           <CardContent>
+                <div className={classes.cardContent} >
+                    <Typography variant="h5" gutterBottom >
+                        {product.name}
+                    </Typography>
+                    <Typography variant="h5" >
+                        {product.price}
+                    </Typography>
+               </div>
+               <Typography variant="h2" color="textSecondary" >
+                    {product.description}
+               </Typography>
+           </CardContent>
+           <CardActions disableSpacing className={classes.cardActions} >
+               <IconsButton aria-label="add to cart" >
+                   <AddShoppingCart />
+               </IconsButton>
+           </CardActions>
+       </Card> 
     )
 }
 
