8e826ccaac321b31577d3bd8fb1a691f563bb1c9
Braxton Harley <braxton.2012.harley@gmail.com>
2020-12-31 06:45:29 -0600

    fixed product display issue

--- src/components/Products/Product/Product.jsx
+++ src/components/Products/Product/Product.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import { Card, CardMedia, CardContent, CardActions, Typography, IconsButton } from '@material-ui/core';
+import { Card, CardMedia, CardContent, CardActions, Typography, IconButton } from '@material-ui/core';
 import { AddShoppingCart }  from '@material-ui/icons';
 
 import useStyles from './styles';
 
-const Product = ( product ) => {
+const Product = ({ product }) => {
     const classes = useStyles();
-    
+
     return (
        <Card className={classes.root}>
            <CardMedia className={classes.media} image='' title={product.name} />
@@ -19,14 +19,13 @@ const Product = ( product ) => {
                         {product.price}
                     </Typography>
                </div>
-               <Typography variant="h2" color="textSecondary" >
-                    {product.description}
-               </Typography>
+               <Typography variant="h2" color="textSecondary" > {product.description} </Typography>
            </CardContent>
+           
            <CardActions disableSpacing className={classes.cardActions} >
-               <IconsButton aria-label="add to cart" >
+               <IconButton aria-label="Add To Cart" >
                    <AddShoppingCart />
-               </IconsButton>
+               </IconButton>
            </CardActions>
        </Card> 
     )
