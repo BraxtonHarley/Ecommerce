f3cd32b882a1a3ae27d8c0e11ebb869a3ff3f23a
Braxton Harley <braxton.2012.harley@gmail.com>
2021-01-03 10:15:05 -0600

    made cart item/cart item styles

--- src/components/Cart/CartItem/CartIem.jsx
+++ src/components/Cart/CartItem/CartIem.jsx
@@ -1,10 +1,29 @@
 import React from 'react';
+import { Typography, Button, Card, CardActions, CardContent, CardMedia } from '@material-ui/core';
+
+import useStyles from './styles';
+import CartItem from './CartItem/CartItem';
 
 const CartIem = () => {
+    const classes = useStyles(); 
+
     return (
-        <div>
-            
-        </div>
+        <Card>
+            <CardMedia image={item.media.source} alt={item.name} className={classes.media} />
+            <CardContent className={classes.cardContent}>
+                <Typography variant='h4'>{item.name}</Typography>
+                <Typography variant='h4'>{item.line_total.formatted_with_symbol}</Typography>
+            </CardContent>
+
+            <CardActions className={classes.cardActons}>
+                <div className={classes.buttons} >
+                    <Button type='button' size='small'>-</Button>
+                    <Typography>{item.quantity}</Typography>
+                    <Button type='button' size='small'>+</Button>
+                </div>
+                <Button variatn='contained' type='button' color='secondary'>Remove</Button>
+            </CardActions>
+        </Card>
     )
 }
 
