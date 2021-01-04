f20418164ab16eaefa09f96383f588b8c4489ca9
Braxton Harley <braxton.2012.harley@gmail.com>
2021-01-04 07:41:05 -0600

    added steps to checkout and styles to checkout

--- src/components/CheckoutForm/Checkout/Checkout.jsx
+++ src/components/CheckoutForm/Checkout/Checkout.jsx
@@ -1,14 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Paper, Stepper, Step, StepLabel, Typography, CircularProgress, Divider, Button } from '@material-ui/core'; 
 import classes from '*.module.css';
 
+const steps =  ['Shipping address', 'Payment details' ];
+
 const Checkout = () => {
+    const [activeStep,  setActiveStep] = useState(0); 
     return (
         <>
             <div  className={classes.toolbar} />
             <main className={classes.layout}>
                 <Paper className={classes.paper}>
                     <Typography variant='h4'  align='center'>Checkout</Typography>
+                    <Stepper activeStep={activeStep} className={classes.stepper}>
+                        { steps.map((step) => (
+                            <Step key={step}>
+                                <StepLabel>{step}</StepLabel>
+                            </Step>
+                        )) }
+                    </Stepper>
                 </Paper>
             </main>
         </>
